8e4ba594e632dab31c06025bfdfaf906334f4064
U-SHIGR\Nbr_Shimada <Nbr_Shimada@1707N0030.shi.co.jp>
2019-06-28 17:07:56 +0900

    =chg scoop_install

--- ps_scoop_install.ps1
+++ ps_scoop_install.ps1
@@ -1,17 +1,15 @@
 #----------------------------------------------------------------------
-# proxy set
+# proxy set id download
 #----------------------------------------------------------------------
 [net.webrequest]::defaultwebproxy = new-object net.webproxy "http://yksproxy.shi.co.jp:8080"
 [net.webrequest]::defaultwebproxy.credentials = [net.credentialcache]::defaultcredentials
 [net.webrequest]::defaultwebproxy.credentials = new-object net.networkcredential 'Nbr_Shimada@shi.co.jp', '05j151'
 
-
-$scoop_install_dir = 'C:\Scoop'
-Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-
 #----------------------------------------------------------------------
 # scoop body
 #----------------------------------------------------------------------
+$scoop_install_dir = 'C:\Scoop'
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 [environment]::setEnvironmentVariable('SCOOP',$scoop_install_dir,'User')
 $env:SCOOP=$scoop_install_dir
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
@@ -23,3 +21,7 @@ scoop bucket add extras
 scoop bucket add scoop-for-jp https://github.com/rkbk60/scoop-for-jp
 scoop bucket add scoop-iyokan-jp https://github.com/tetradice/scoop-iyokan-jp
 
+#----------------------------------------------------------------------
+# proxy set scoop
+#----------------------------------------------------------------------
+scoop config proxy 'Nbr_Shimada\@shi.co.jp:05j151@yksproxy.shi.co.jp:8080'
